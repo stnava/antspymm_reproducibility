bb37ef508ecce6660875cd1df2d5540bf47d6c95
stnava <stnava@gmail.com>
2023-12-25 21:57:46 -0500

    ENH: joint data

--- src/icck.R
+++ src/icck.R
@@ -1,4 +1,13 @@
 
+## Intraclass Correlation Coefficients with  95 % C.I.
+##            Model         Measures  Type    ICC Lower CI Upper CI
+## 1 one-way random        Agreement  ICC1 0.1286  0.05437   0.2936
+## 2 two-way random        Agreement  ICC2 0.1672  0.07871   0.3408
+## 3  two-way fixed      Consistency  ICC3 0.8266  0.72401   0.9177
+## 4 one-way random   Avg. Agreement ICC1k 0.7264  0.50861   0.8821
+## 5 two-way random   Avg. Agreement ICC2k 0.7832  0.60597   0.9030
+## 6  two-way fixed Avg. Consistency ICC3k 0.9885  0.97926   0.9950
+
 icck <- function( indf, outcome, k=6, lmeru=FALSE, agree=FALSE  ) {
   # see: https://journals.plos.org/plosone/article/file?id=10.1371/journal.pone.0219854&type=printable
   # see: QUANTIFYING TEST-RETEST RELIABILITY USING THE INTRACLASS CORRELATION COEFFICIENT AND THE SEM
